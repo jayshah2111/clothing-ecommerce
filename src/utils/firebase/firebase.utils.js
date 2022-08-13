2b7e1cf8d7aa1c8e97ec2db850cf7d61f908a53c
Jay <shahjay2111@gmail.com>
2022-08-13 19:29:47 +0530

    Created sign-in and sign-up pages with authentication

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,14 +3,16 @@ import {
     getAuth,
     signInWithRedirect,
     signInWithPopup,
-    GoogleAuthProvider
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
  } from 'firebase/auth';
  import {
     getFirestore,
     doc,
     getDoc,
     setDoc
- } from 'firebase/firestore'
+ } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCaekxoH2pbgt6L_hn_eLFeNxoZc94bMGY",
@@ -33,9 +35,14 @@ const firebaseConfig = {
   export const signInWithGooglePopup = () => {
     return signInWithPopup(auth, provider);
   }
+  export const signInWithGoogleRedirect = () => {
+    signInWithRedirect(auth, provider);
+  }
 
   export const db = getFirestore();
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {
+    if(!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
 
@@ -48,13 +55,26 @@ const firebaseConfig = {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
-            })
+                createdAt,
+                ...additionalInformation
+            });
         }catch(error){
             console.log('error creating the user', error.message);
         }
     }
-
     return userDocRef;
-    
   };
+
+  export const createAuthUserWithEmailAndPassWord = async (email, password) => {
+    if(!email || !password){
+      return;
+    }
+    return await createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  export const signInAuthUserWithEmailAndPassWord = async (email, password) => {
+    if(!email || !password){
+      return;
+    }
+    return await signInWithEmailAndPassword(auth, email, password);
+  }
